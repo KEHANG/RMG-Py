86773a7939c9cae6428e023b17704b80a1cf262b
Nick Vandewiele <nickvandewiele@gmail.com>
2015-10-01 16:30:12 -0400

    use inchi sorting in createULayer
    generate the u-layer based on an atom order from the inchi
    canonicalization algorithm.
    
    - remove H's from original molecule (deep copy expected)
    - generate aux info, extract the atom numbers
    - sort the heavy atoms of the original mol with the inchi numbers
    - generate u-layer based on ordered mol

--- rmgpy/molecule/parser.py
+++ rmgpy/molecule/parser.py
@@ -370,7 +370,6 @@ def fromAugmentedInChI(mol, aug_inchi):
 
     mol.multiplicity = aug_inchi.mult
 
-
     #triplet to singlet conversion
     if mol.multiplicity == 1 and mol.getNumberOfRadicalElectrons() == 2:
         for at in mol.atoms:
@@ -391,8 +390,6 @@ def fromAugmentedInChI(mol, aug_inchi):
 
 
     # unsaturated bond to triplet conversion
-    # sort the atoms so that they adopt the order of RMG's algorithm, which was used for the u indices:
-    sortAtoms(mol)
     correct = check_number_unpaired_electrons(mol)
 
     unsaturated = isUnsaturated(mol)
@@ -410,7 +407,6 @@ def fromAugmentedInChI(mol, aug_inchi):
         unsaturated = isUnsaturated(mol)
 
     check(mol, aug_inchi)
-    sortAtoms(mol)
     return mol
 
 def fromSMILES(mol, smilesstr, backend='try-all'):
@@ -545,7 +541,9 @@ def fromOBMol(mol, obmol):
 
     
     # Set atom types and connectivity values
-    mol.update()
+    mol.updateConnectivityValues()
+    mol.updateAtomTypes()
+    mol.updateMultiplicity()
     mol.updateLonePairs()
     
     # Assume this is always true
@@ -611,10 +609,6 @@ def toOBMol(mol):
     # cython.declare(atom=Atom, atom1=Atom, bonds=dict, atom2=Atom, bond=Bond)
     # cython.declare(index1=cython.int, index2=cython.int, order=cython.int)
 
-    # Sort the atoms before converting to ensure output is consistent
-    # between different runs
-    sortAtoms(mol)
-
     atoms = mol.vertices
 
     obmol = openbabel.OBMol()
@@ -655,9 +649,6 @@ def toRDKitMol(mol, removeHs=True, returnMapping=False, sanitize=True):
                    index2=cython.int,
                    )
                    
-    # Sort the atoms before converting to ensure output is consistent
-    # between different runs
-    sortAtoms(mol)
     atoms = mol.vertices
     rdAtomIndices = {} # dictionary of RDKit atom indices
     rdkitmol = Chem.rdchem.EditableMol(Chem.rdchem.Mol())
@@ -723,6 +714,46 @@ def createULayer(mol):
 
     Returns None if the molecule bears less than 2 unpaired electrons
     """
+
+    if mol.getRadicalCount() == 0:
+        return None
+    elif mol.getFormula() == 'H':
+        return U_LAYER_PREFIX+str(1)
+
+    # remove hydrogens
+    hydrogens = [at for at in mol.atoms if at.number == 1]
+    [mol.removeAtom(h) for h in hydrogens]
+            
+    # sort the atoms based on the inchi canonicalization algorithm
+    m = toRDKitMol(mol)
+
+    # generate inchi and auxiliary info
+    inchi , auxinfo = Chem.MolToInchiAndAuxInfo(m)
+
+    # extract the atom numbers
+    pieces = auxinfo.split('/')
+    original_atom_numbers = None
+    for piece in pieces:
+        if piece.startswith('N'):
+            original_atom_numbers = piece
+            break
+
+    assert original_atom_numbers is not None, "inchi: {}, {}".format(inchi, auxinfo)
+    """
+    definition of N-list: 
+
+    The original number of an atom with identification number n is given as the
+    n-th member of this list for a component; the lists are separated with “;”. 
+    """
+    Nlist = map(int, original_atom_numbers[2:].split(','))
+    new_indices = [Nlist.index(i+1) for i,atom in enumerate(mol.atoms)]
+
+    # sort the atoms based on the new inchi order
+    mol.atoms = [x for (y,x) in sorted(zip(new_indices,mol.atoms), key=lambda pair: pair[0])]
+
+    # find the resonance isomer with the lowest u index:
+    mol = normalize(mol)
+
     ulayer = [str(i+1) for i, at in enumerate(mol.atoms) if at.radicalElectrons > 0]
     if ulayer:
         return (U_LAYER_PREFIX + ','.join(ulayer))
@@ -738,11 +769,9 @@ def toAugmentedInChI(mol):
     Separate layer with a forward slash character.
     """
     mol_copy = mol.copy(deep=True)
-    mol_copy = normalize(mol_copy)
-    sortAtoms(mol_copy)
     inchi = toInChI(mol_copy)
-
     mult = createMultiplicityLayer(mol_copy.multiplicity)    
+
     ulayer = createULayer(mol_copy)
 
     return compose_aug_inchi(inchi, mult, ulayer)
@@ -871,24 +900,6 @@ def updateAtomConnectivityValues(mol):
     connectivityValues = mol.update_recurse([atom.number * len(atom.bonds) for atom in mol.atoms], 0)
     for atom, value in zip(mol.atoms, connectivityValues):
         atom.connectivity = value
-
-def sortAtoms(mol):
-    """
-    Sort the atoms in the graph. This can make certain operations, e.g.
-    the isomorphism functions, much more efficient.
-    """
-    cython.declare(a=Atom)
-    for a in mol.atoms:
-        if a.sortingLabel != 2: break
-    else:
-        return
-        
-    updateAtomConnectivityValues(mol)
-    mol.atoms.sort(key=lambda a: a.getDescriptor())
-    moveHs(mol)
-
-    for a in mol.atoms:
-        a.sortingLabel = 2
 
 def normalize(mol):
     """
